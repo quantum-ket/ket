04d3ebf4e0b6db796708a5a0d3809e34fac3f7a1
Evandro Chagas <ev.crr97@gmail.com>
2020-07-12 11:49:24 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
-from setuptools import setup
-from setuptools.extension import Extension
+from skbuild import setup
+from setuptools import find_namespace_packages
 from os import listdir
 
 with open('README.md', 'r') as file:
     long_description = file.read()
 
-libket_src = ['libket/src/'+file for file in listdir('libket/src')]
-libket_src.append('build/ket/ketPYTHON_wrap.cxx')
-
-ext_module = Extension('ket._ketpy',
-        sources=libket_src, 
-        extra_compile_args=['-std=c++17'],
-        )
+setup_requirements = ['scikit-build>=0.11.1',
+                      'cmake>=3.15',
+                      'conan>=1.25'
+                      ]
 
 setup (name = 'ket',
-       version='0.1rc1',
+       version='0.1b',
+       cmake_source_dir='.',
        author='Evandro Chagas Ribeiro da Rosa',
        author_email='evandro.crr@posgrad.ufsc.br',
        description='Ket quantum programming language library and interpreter',
        long_description=long_description,
        long_description_content_type='text/markdown',
        url='https://gitlab.com/quantum-ket/ket',
-       ext_modules = [ext_module],
-       packages=['build/ket'],
-       classifiers=[
-            'Programming Language :: Python :: 3',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-        ]
+       packages=find_namespace_packages(),
+       setup_requires=setup_requirements,
+       classifiers=['Programming Language :: Python :: 3',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: MIT License',
+                   ]
        )
