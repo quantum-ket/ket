ecee3ebec678e5ae77e0b6f6ea0118bbaa134287
Evandro Chagas <ev.crr97@gmail.com>
2021-04-06 11:16:56 -0300

    update examples

--- examples/teleport.py
+++ examples/teleport.py
@@ -3,20 +3,24 @@ from ket.lib import bell
 
 @code_ket
 def teleport(alice : quant) -> quant:
-    alice_b, bob_b = bell(0,0)
+    alice_b, bob_b = bell()
+
     ctrl(alice, x, alice_b)
     h(alice)
+
     m0 = measure(alice)
     m1 = measure(alice_b)
+
     if m1 == 1:
         x(bob_b)
     if m0 == 1:
         z(bob_b)
+
     return bob_b
 
 alice = quant(1)         # alice = |0⟩
 h(alice)                 # alice = |+⟩
-z(alice)                 # alice = |-⟩
+z(alice)                 # alice = |–⟩
 bob = teleport(alice)    # bob  <- alice
 h(bob)                   # bob   = |1⟩
 bob_m = measure(bob)
