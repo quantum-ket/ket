9790f99ab435b6a2a19044f4a369d1588895f606
Evandro Chagas <ev.crr97@gmail.com>
2021-05-26 11:04:19 -0300

    add ctrl doc

--- ket/standard/ctrl.py
+++ ket/standard/ctrl.py
@@ -15,7 +15,7 @@
 
 from ..ket import ctrl_begin, ctrl_end, X
 from ..types import quant
-from typing import Iterable, List, Union, Callable, Optional
+from typing import Iterable, List, Union, Callable
 from functools import reduce
 
 class control:
@@ -86,7 +86,87 @@ def _get_qubits_at(control):
         return None
 
 def ctrl(control : Union[Iterable[quant], quant, slice, int, Iterable[int]], func : Union[Callable, Iterable[Callable]] , *args, **kwargs):
-    """Add qubits of controll to a operation call."""
+    r"""Add controll-qubits to a Callable
+
+    **Call with control qubits**
+
+    * ``control`` must be :class:`~ket.types.quant` or :class:`~ket.types.quant` iterable.
+    * ``func`` must be ``Callable`` or ``Callable`` iterable.
+
+    .. code-block:: ket
+
+        ret1 = ctrl(control_qubits, func, *args, **kwargs)
+        # Equivalent to:
+        # with control(control_qubits):
+        #     ret1 = func(*args, **kwargs)
+
+        ret2 = ctrl([q0, q1, q2, ...], func, *args, **kwargs)
+        # Equivalent to:
+        # with control(*control):
+        #     ret2 = func(*args, **kwargs)
+        
+        ret3 = ctrl(control_qubits, [f0, f1, f2, ...], *args, **kwargs)
+        # Equivalent to:
+        # ret3 = []
+        # with control(control_qubits):
+        #     for f in func:
+        #     ret3.append(f(*args, **kwargs))
+ 
+    **Create controlled-operation**
+
+    1. If the keyword argument ``later_call`` is ``True``, return a
+    ``Callable[[], Any]``:
+
+    .. code-block:: ket
+
+        ctrl_func = ctrl(control_qubits, func, *args, **kwargs, later_call=True)
+        # Equivalent to:
+        # ctrl_func = lambda : ctrl(control_qubits, func, *args, **kwargs)
+
+    **Example:**
+
+    .. code-block:: ket
+        :emphasize-lines: 8
+
+        def increment(q):
+            if len(q) > 1:
+                ctrl(q[-1], increment, q[:-1])
+            X(q[-1])
+
+        size = ceil(log2(len(inputs)))+1
+        with quant(size) as inc:
+            with around(ctrl(q , increment, inc, later_call=True) for q in inputs):
+                with control(inc, on_state=len(inputs)//2):
+                    X(output)
+            inc.free()
+
+    2. If ``control`` and ``target`` qubits are ``int``, ``slice``, or
+    ``Iterable[int]``,  return a ``Callable[[quant], Any]``:
+
+    .. code-block:: ket
+
+        ctrl_func = ctrl(ctrl_index, func, target_index)
+        # Equivalent to:
+        # ctrl_func = lambda q : ctrl(q[ctrl_index], func, q[target_index])
+
+    **Example:**
+
+    .. code-block:: ket
+
+        with around(ctrl(0, X, slice(1, None)), q): # ctrl(q[0], X, q[1:]) 
+            H(q[0])                                 # H(q[0])
+                                                    # ctrl(q[0], X, q[1:])
+ 
+    Args:
+        control: Control qubits, use :class:`~ket.types.quant` for call with control qubits and index to create controlled-operations.
+        func: Function or list of functions to add control.
+
+    Other Parameters:
+        target (``Union[slice, int, Iterable[int]]``): Target qubits to create controlled-operations.
+
+    Keyword Args:
+        later_call (``bool``): If ``True``, do not execute and return a ``Callable[[], Any]``.
+    """
 
     q_ctrl = _get_qubits_at(control)
     if q_ctrl is not None:
