f82fa3d4859145e74ca742bae7aacdea2147894d
Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
2023-03-15 12:07:04 -0300

    update ci

--- scripts/make_wheel.sh
+++ scripts/make_wheel.sh
@@ -1,3 +1,3 @@
-#/bin/bash
-docker build -t ket_wheel -f scripts/make.dockerfile .
+#!/bin/sh
+docker build -t ket_wheel -f scripts/make_wheel.dockerfile .
 docker run -v $PWD:/mnt --rm ket_wheel /bin/bash -c "mkdir -p /mnt/wheelhouse && cp ket*.whl /mnt/wheelhouse"
