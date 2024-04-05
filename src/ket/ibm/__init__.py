18c84730524797c074bcb108e92d05225311916a
oasjs <otavio.jatoba@grad.ufsc.br>
2024-04-05 19:57:22 -0300

    Add init file to ibm submodule

---
+++ src/ket/ibm/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2020 Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
+# SPDX-FileCopyrightText: 2020 Rafael de Santiago <r.santiago@ufsc.br>
+# SPDX-FileCopyrightText: 2024 Otávio Augusto de Santana Jatobá
+# <otavio.jatoba@grad.ufsc.br>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .qiskit_interface import QiskitInterface
