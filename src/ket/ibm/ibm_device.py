62e5199098a82e8add7db216b70c5b156496b7ac
oasjs <otavio.jatoba@grad.ufsc.br>
2024-04-10 13:44:26 -0300

    Rename QiskitInterface back to IBMDevice and add documentation

--- src/ket/ibm/ibm_device.py
+++ src/ket/ibm/ibm_device.py
@@ -1,42 +1,53 @@
-"""IBM Quantum Devices backend for Ket"""
+""" Interface for setting up connection to the IBM quantum infrastructure. """
+
+try:
+    from qiskit.providers import Backend
+    from qiskit_ibm_runtime import QiskitRuntimeService
+except ImportError as exc:
+    raise ImportError(
+        "IBMDevice requires the qiskit module to be used. You can install them"
+        "alongside ket by running `pip install ket[ibm]`."
+    ) from exc
 
 import json
 from ctypes import CFUNCTYPE, POINTER, c_uint8, c_size_t
 from ..clib.libket import BatchCExecution, API as libket
-from .qiskit_client import QiskitClient
-
-from qiskit.providers import Backend
-from qiskit_ibm_runtime import QiskitRuntimeService
+from .ibm_client import IBMClient
 
 
-class QiskitInterface:
+class IBMDevice:
+    """Sets up a ket process to connect to the IBM Quantum or IBM Cloud devices."""
+
     def __init__(
         self, num_qubits: int, backend: Backend, service: QiskitRuntimeService | None
     ) -> None:
         self.num_qubits = num_qubits
+        self.client = IBMClient(num_qubits, backend, service)
 
         self.result_json = None
         self.result_json_len = None
-
-        self.client = QiskitClient(num_qubits, backend, service)
         self._formatted_result = None
 
         @CFUNCTYPE(None, POINTER(c_uint8), c_size_t)
         def submit_execution(data, size):
+            """Sends the ket circuit instructions from libket to the IBM Client."""
             instructions = json.loads(bytearray(data[:size]))
             self._formatted_result = self.client.process_instructions(instructions)
 
         @CFUNCTYPE(None, POINTER(POINTER(c_uint8)), POINTER(c_size_t))
         def get_result(result_ptr, size):
+            """Sends the processing result from the IBM Client back to libket."""
             result_dict = self._formatted_result
             result_json = json.dumps(result_dict).encode("utf-8")
             result_len = len(result_json)
 
             self.result_json = (c_uint8 * result_len)(*result_json)
             self.result_json_len = result_len
+            # Set the result pointer and size, both of which must remain valid inside
+            # python until the libket process has finished. That's why they're class
+            # attributes.
             result_ptr[0] = self.result_json
             size[0] = self.result_json_len
-            return
 
         @CFUNCTYPE(c_uint8)
         def get_status():
@@ -49,7 +60,7 @@ class QiskitInterface:
         )
 
     def make_configuration(self):
-        """Make configuration"""
+        """Set up the configuration for the ket process inside libket."""
 
         return libket["ket_batch_make_configuration"](
             self.num_qubits,
