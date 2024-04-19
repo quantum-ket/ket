865cbd681c124704c9a627b596d8b4db79f3bde5
oasjs <otavio.jatoba@grad.ufsc.br>
2024-04-19 13:53:17 -0300

    Refactor test file to include all tests on the IBMDevice class and its functionality

--- tests/ibm/test_ibm_device.py
+++ tests/ibm/test_ibm_device.py
@@ -1,86 +1,110 @@
-import ket
+""" Test module for the IBMDevice class """
+
+# SPDX-FileCopyrightText: 2024 Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
+# SPDX-FileCopyrightText: 2024 Otávio Augusto de Santana Jatobá
+# <otavio.jatoba@grad.ufsc.br>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+try:
+    import qiskit
+    from qiskit_aer import AerSimulator
+    from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+except ImportError as exc:
+    raise ImportError(
+        "IBMDevice requires the qiskit module to be used. You can install them"
+        "alongside ket by running `pip install ket[ibm]`."
+    ) from exc
+
 import math
-import qiskit
-from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-from qiskit_aer import AerSimulator
+import ket
 from ket.ibm import IBMDevice
 
-num_qubits = 3
-aer_sim = AerSimulator()
+
+BACKEND = AerSimulator()
+NUM_QUBITS = 3
 
 
-def test_measure():
-    device = IBMDevice(num_qubits=num_qubits, backend=aer_sim, service=None)
-    ket_process = ket.Process(device.make_configuration())
-    q = ket_process.alloc(3)
-    ket.CNOT(ket.H(q[0]), q[1])
-    ket.CNOT(ket.H(q[1]), q[2])
-    ket.measure(q)
-    ket_process.execute()
+class TestIBMDevice:
+    """Test class for the IBMDevice use cases."""
 
-    qiskit_circuit = qiskit.QuantumCircuit(num_qubits, num_qubits)
-    qiskit_circuit.h(2)
-    qiskit_circuit.cx(2, 1)
-    qiskit_circuit.h(1)
-    qiskit_circuit.cx(1, 0)
-    qiskit_circuit.measure([0, 1, 2], [0, 1, 2])
+    def _build_qiskit_bell_circuit(self):
+        qiskit_circuit = qiskit.QuantumCircuit(NUM_QUBITS, NUM_QUBITS)
+        qiskit_circuit.h(0)
+        qiskit_circuit.cx(0, 1)
+        return qiskit_circuit
 
-    pm = generate_preset_pass_manager(backend=aer_sim, optimization_level=1)
-    isa_qc = pm.run(qiskit_circuit)
+    def test_measure(self):
+        """Tests the measurement operation using the IBMDevice"""
 
-    for interface_inst in device.client.qiskit_builder.circuit.data:
-        assert interface_inst in isa_qc.data
+        device = IBMDevice(backend=BACKEND, num_qubits=NUM_QUBITS)
+        ket_process = ket.Process(device.make_configuration())
+        q = ket_process.alloc(NUM_QUBITS)
+        ket.CNOT(ket.H(q[0]), q[1])
+        ket.measure(q)
+        ket_process.execute()
 
-    assert True
+        qubits = list(range(NUM_QUBITS))
+        qiskit_circuit = self._build_qiskit_bell_circuit()
+        qiskit_circuit.measure(qubits, qubits)
 
+        pm = generate_preset_pass_manager(backend=BACKEND, optimization_level=1)
+        isa_qc = pm.run(qiskit_circuit)
 
-def test_sample():
-    device = IBMDevice(num_qubits=num_qubits, backend=aer_sim, service=None)
-    ket_process = ket.Process(device.make_configuration())
-    q = ket_process.alloc(3)
-    ket.CNOT(ket.H(q[0]), q[1])
-    ket.CNOT(ket.H(q[1]), q[2])
-    ket.sample(q)
-    ket_process.execute()
+        for interface_inst in device.circuit.data:
+            assert interface_inst in isa_qc.data
 
-    qiskit_circuit = qiskit.QuantumCircuit(num_qubits, num_qubits)
-    qiskit_circuit.h(2)
-    qiskit_circuit.cx(2, 1)
-    qiskit_circuit.h(1)
-    qiskit_circuit.cx(1, 0)
-    qiskit_circuit.measure([0, 1, 2], [0, 1, 2])
+        assert True
+        print("Measurement test passed!")
 
-    pm = generate_preset_pass_manager(backend=aer_sim, optimization_level=1)
-    isa_qc = pm.run(qiskit_circuit)
+    def test_sample(self):
+        """Tests the sample operation using the IBMDevice"""
 
-    for interface_inst in device.client.qiskit_builder.circuit.data:
-        assert interface_inst in isa_qc.data
+        device = IBMDevice(backend=BACKEND, num_qubits=NUM_QUBITS)
+        ket_process = ket.Process(device.make_configuration())
+        q = ket_process.alloc(NUM_QUBITS)
+        ket.CNOT(ket.H(q[0]), q[1])
+        ket.sample(q)
+        ket_process.execute()
 
-    assert True
+        qubits = list(range(NUM_QUBITS))
+        qiskit_circuit = self._build_qiskit_bell_circuit()
+        qiskit_circuit.measure(qubits, qubits)
 
+        pm = generate_preset_pass_manager(backend=BACKEND, optimization_level=1)
+        isa_qc = pm.run(qiskit_circuit)
 
-def test_exp_value():
-    ket_result = isolate_processing("ket")
-    device_result = isolate_processing("device")
-    print(f"ket_result: {ket_result}")
-    print(f"device_result: {device_result}")
+        for interface_inst in device.circuit.data:
+            assert interface_inst in isa_qc.data
 
-    assert math.isclose(ket_result, device_result, rel_tol=1e-9)
+        assert True
+        print("Sample test passed!")
 
+    def test_exp_value(self):
+        """Tests the expectation value operation using the IBMDevice"""
 
-def isolate_processing(source):
-    device = IBMDevice(num_qubits=num_qubits, backend=aer_sim, service=None)
-    p = ket.Process() if source == "ket" else ket.Process(device.make_configuration())
-    q = p.alloc(num_qubits)
-    ket.CNOT(ket.H(q[0]), q[1])
-    ket.CNOT(ket.H(q[1]), q[2])
-    hamiltonian = (ket.Pauli("X", q) * ket.Pauli("Z", q) * ket.Pauli("Y", q)) + (
-        2 * ket.Pauli("Y", q) * ket.Pauli("X", q)
-    )
-    return ket.exp_value(hamiltonian).value
+        ket_process = ket.Process()
+        q = ket_process.alloc(NUM_QUBITS)
+        ket.CNOT(ket.H(q[0]), q[1])
+        hamiltonian = ket.Pauli("Z", q)
+
+        device = IBMDevice(backend=BACKEND, num_qubits=NUM_QUBITS)
+        ibm_process = ket.Process(device.make_configuration())
+        t = ibm_process.alloc(NUM_QUBITS)
+        ket.CNOT(ket.H(t[0]), t[1])
+
+        ket_result = ket.exp_value(hamiltonian).value
+        device_result = ket.exp_value(hamiltonian).value
+
+        ket_process.execute()
+        ibm_process.execute()
+
+        assert math.isclose(ket_result, device_result, rel_tol=1e-9)
+        print("Expectation value test passed!")
 
 
 if __name__ == "__main__":
-    test_measure()
-    test_sample()
-    test_exp_value()
+    test = TestIBMDevice()
+    test.test_measure()
+    test.test_sample()
+    test.test_exp_value()
